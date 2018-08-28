53e9da8246f82041d2a50ab150a3cef81d7c4752
Anton Smirnov <smiranton.kiev@gmail.com>
2018-08-28 08:43:18 +0300

    Check CI

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var proxy = require('./middlewares/proxy');
 var app = express();
 
 app.use(cors);
-app.use('/api', proxy);
+// app.use('/api', proxy);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(browserHistory);
 
